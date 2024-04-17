f37cd130c4a22f03854309c8b1450e67388f790a
XingjianL <lixingjian2002@gmail.com>
2024-04-16 23:43:10 -0400

    buoy randomization prep

---
+++ Assets/scripts/Objectives/buoyRandomization.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buoyRandomization : MonoBehaviour
+{
+    public GameObject buoy;
+    public Material mat;
+    public void Randomize(){
+
+    }
+}
