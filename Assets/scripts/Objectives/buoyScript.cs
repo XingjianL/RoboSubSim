ebd345f02c8181a68d7caf3f881184268e5ba63f
XingjianL <lixingjian2002@gmail.com>
2024-05-03 15:47:58 -0400

    buoy model

---
+++ Assets/scripts/Objectives/buoyScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buoyScript : MonoBehaviour
+{
+    public GameObject buoy;
+    public Material mat;
+    public void Randomize(){
+
+    }
+}
